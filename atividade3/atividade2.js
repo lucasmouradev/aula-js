fa2ba477c55ecc71a7febe636e6c6d477118188a
lucas <lucasmouraadev@gmail.com>
2022-06-11 02:51:24 -0300

    inclusao da aula sobre JS da DIO

---
+++ atividade3/atividade2.js
@@ -0,0 +1,23 @@
+function calculaIdade(anos) {
+    return `Daqui a ${anos} anos, ${this.nome} téra ${this.idade + anos} anos de idade .`;
+}
+
+
+const pessoa1 = {
+    nome: 'Maria',
+    idade: 30,
+};
+
+
+const pessoa2 = {
+    nome: 'Carla',
+    idade: 26,
+};
+
+const animal = {
+    nome: 'Fiona',
+    idade: 5,
+    raca: 'Pug',
+};
+
+console.log(calculaIdade.apply(animal, [7]));
